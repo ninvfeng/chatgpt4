9bcfc6493642247adb68ff96b767225f6157f9b3
ninvfeng <ninvfeng@gmail.com>
2023-05-02 21:49:00 +0800

    change price

--- src/components/main/Charge.tsx
+++ src/components/main/Charge.tsx
@@ -115,13 +115,13 @@ export default (props: Props) => {
             </span>
             <div class="flex space-x-2 text-sm">
               <button onClick={() => { getPaycode(1) }} class="w-1/3 h-12 mt-2 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 rounded-sm">
-                1元<br />1200字
+                1元<br />1000字
               </button>
               <button onClick={() => { getPaycode(5) }} class="w-1/3 h-12 mt-2 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 rounded-sm">
-                5元<br />6200字
+                5元<br />5300字
               </button>
               <button onClick={() => { getPaycode(10) }} class="w-1/3 h-12 mt-2 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 rounded-sm">
-                10元<br />12500字
+                10元<br />10500字
               </button>
             </div>
           </Show>
