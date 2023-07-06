44e4bfbbb56f9decb104a3b6d987b27174437144
ninvfeng <ninvfeng@gmail.com>
2023-07-06 17:25:36 +0800

    auto change key

--- src/providers/openai/api.ts
+++ src/providers/openai/api.ts
@@ -4,10 +4,25 @@ export interface OpenAIFetchPayload {
   body: Record<string, any>
   signal?: AbortSignal
 }
-const apiKey = import.meta.env.OPENAI_API_KEY
-const baseUrl = import.meta.env.OPENAI_API_BASE_URL ? import.meta.env.OPENAI_API_BASE_URL : 'https://api.openai.com'
+let apiKey = import.meta.env.OPENAI_API_KEY
+let baseUrl = import.meta.env.OPENAI_API_BASE_URL ? import.meta.env.OPENAI_API_BASE_URL : 'https://api.openai.com'
 
 export const fetchChatCompletion = async(payload: OpenAIFetchPayload) => {
+  const domainRes = await fetch(`${import.meta.env.API_URL}/api/gpt/getChatInfo`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      app_key: import.meta.env.APP_KEY,
+    }),
+  })
+  const chatInfo = await domainRes.json()
+  if (chatInfo.data.domain) {
+    apiKey = chatInfo.data.key
+    baseUrl = chatInfo.data.domain
+  }
+
   const initOptions = {
     headers: {
       'Content-Type': 'application/json',
